7bf5b54b94e064b396b4a256e0b16f94a1a54d29
Zbigniew Tomanek <zbiginewt@gmail.com>
2019-10-20 20:41:57 +0200

    new functions

--- plot_points.py
+++ plot_points.py
@@ -1,5 +1,7 @@
 from matplotlib import pyplot as plt
 import numpy as np
+import scipy.stats as stats
+
 
 def sin():
     x = np.arange(0, 10, 0.2)
@@ -19,5 +21,38 @@ def sin():
     plt.show()
 
 
+def normal_distr():
+    y = np.random.normal(loc=0, size=50)
+    fig, ax = plt.subplots()
+    ax.scatter(np.arange(50), y)
+
+    plt.title('Normal distribution')
+    plt.show()
+
+
+def random_points():
+    N = 50
+    x = np.random.rand(N)
+    y = np.random.rand(N)
+    plt.scatter(x, y)
+    plt.show()
+
+
+def plot_gaussian_curve():
+    mu = 0
+    variance = 1
+    sigma = np.sqrt(variance)
+
+    x = np.linspace(mu - 3 * sigma, mu + 3 * sigma, 100)
+    plt.plot(x, stats.norm.pdf(x, mu, sigma))
+    plt.show()
+
+
+def euler(x0, delta, a):
+    x = np.linspace(x0, x0 + delta)
+    plt.plot(x, a * np.exp(a * x))
+    plt.show()
+
+
 if __name__ == '__main__':
-    sin()
+    euler(5, 10, -0.2)
